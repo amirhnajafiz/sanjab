64293ba0d884ded7eb874cb96fbfdd1c99d0a9f7
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-31 19:38:40 +0330

    init: worker

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -1,5 +1,18 @@
 package worker
 
+import "github.com/amirhnajafiz/sanjab/pkg/enum"
+
+// Worker manages a resource by watching it
+// if a resource is added, it will store in
+// to storage
 type Worker interface {
 	Watch() error
+	State() enum.Status
+}
+
+// Register system workers
+func Register() []Worker {
+	var workers []Worker
+
+	return workers
 }
