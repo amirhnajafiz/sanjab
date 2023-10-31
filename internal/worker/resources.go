de1160f3d2fbc7b6f282696fe9f3b5e1b13542c1
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-31 20:09:19 +0330

    add: base resource watcher

--- internal/worker/resources.go
+++ internal/worker/resources.go
@@ -1,14 +1,41 @@
 package worker
 
-import "github.com/amirhnajafiz/sanjab/pkg/enum"
+import (
+	"context"
+	"log"
 
-func newPodResource(cfg Config) *worker {
+	"github.com/amirhnajafiz/sanjab/pkg/enum"
+
+	cv1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	toolsWatch "k8s.io/client-go/tools/watch"
+)
+
+func newPodResource(client *kubernetes.Clientset, cfg Config) *worker {
 	w := newWorker(enum.PodResource)
 
 	if cfg.Has(w.Resource) {
 		w.Status = enum.PendingStatus
 		w.CallBack = func() error {
-			// some logic
+			watchFunc := func(options v1.ListOptions) (watch.Interface, error) {
+				timeOut := int64(60)
+
+				return client.CoreV1().Pods("").Watch(context.Background(), v1.ListOptions{TimeoutSeconds: &timeOut})
+			}
+
+			watcher, _ := toolsWatch.NewRetryWatcher("1", &cache.ListWatch{WatchFunc: watchFunc})
+
+			for event := range watcher.ResultChan() {
+				item := event.Object.(*cv1.Pod)
+
+				if event.Type == watch.Added {
+					// save item to storage
+					log.Println(item)
+				}
+			}
 
 			return nil
 		}
@@ -17,7 +44,7 @@ func newPodResource(cfg Config) *worker {
 	return w
 }
 
-func newDeploymentResource(cfg Config) *worker {
+func newDeploymentResource(client *kubernetes.Clientset, cfg Config) *worker {
 	w := newWorker(enum.DeploymentResource)
 
 	if cfg.Has(w.Resource) {
--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"os"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	config, _ := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
+	clientset, _ := kubernetes.NewForConfig(config)
+}
