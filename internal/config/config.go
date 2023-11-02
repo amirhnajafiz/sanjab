ba2725173706a2886055e5252086ed22812bb90a
amirhnajafiz <najafizadeh21@gmail.com>
2023-11-02 14:07:04 +0330

    add: storage to configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 
+	"github.com/amirhnajafiz/sanjab/internal/storage"
+
 	"github.com/knadh/koanf"
 	"github.com/knadh/koanf/parsers/yaml"
 	"github.com/knadh/koanf/providers/file"
@@ -12,10 +14,11 @@ import (
 )
 
 type Config struct {
-	Resources []string `koanf:"resources"`
-	Timeout   int      `koanf:"timeout"`
-	Port      int      `koanf:"port"`
-	Namespace string   `koanf:"namespace"`
+	Storage   storage.Config `koanf:"ceph"`
+	Resources []string       `koanf:"resources"`
+	Timeout   int            `koanf:"timeout"`
+	Port      int            `koanf:"port"`
+	Namespace string         `koanf:"namespace"`
 }
 
 func Load(path string) Config {
--- internal/config/default.go
+++ internal/config/default.go
@@ -1,7 +1,15 @@
 package config
 
+import "github.com/amirhnajafiz/sanjab/internal/storage"
+
 func Default() Config {
 	return Config{
+		Storage: storage.Config{
+			Host:   "",
+			Secret: "",
+			Access: "",
+			Bucket: "",
+		},
 		Resources: []string{},
 		Namespace: "",
 		Timeout:   10,
